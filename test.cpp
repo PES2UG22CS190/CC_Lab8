b7c8560fb80749f637b81634190cd17ca3fce194
PES2UG22CS190 <pes2ug22cs190@pesu.pes.edu>
2025-03-13 16:09:18 +0530

    Created test.cpp
    For Task 5

---
+++ test.cpp
@@ -0,0 +1,22 @@
+//File: hello.cpp
+
+// This new file is being created for Ttask 5
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(void) {
+
+  cout << "Hello, World\n";
+
+  cout << "Hello, Jenkins\n";
+
+  cout << "I have successfully built and run this  \n";
+
+  return 0;
+
+}
+
